5366fe5d8eb4143ccee80a569222ee8213750e2f
aapetsi <apetsiampiah@gmail.com>
2021-01-12 05:47:49 +0000

    add some styling to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { Link } from 'react-router-dom'
+import { Button } from '@material-ui/core'
 
 class HomePage extends Component {
   render() {
@@ -12,16 +13,29 @@ class HomePage extends Component {
           <div className='jumbotron jumbotron-fluid py-5'>
             <div className='container text-center py-5'>
               <h1 className='display-4'>Welcome to ChatApp</h1>
-              <p className='lead'>
-                A great place to share your thoughts with friends
-              </p>
+              <p className='lead'>A great place to chat</p>
               <div className='mt-4'>
-                <Link className='btn btn-primary px-5 mr-3' to='/signup'>
-                  Create New Account
-                </Link>
-                <Link className='btn px-5' to='/login'>
-                  Login to Your Account
-                </Link>
+                <Button
+                  style={{ marginRight: '40px' }}
+                  variant='contained'
+                  color='primary'
+                >
+                  <Link
+                    style={{ textDecoration: 'none', color: 'white' }}
+                    to='/signup'
+                  >
+                    Create New Account
+                  </Link>
+                </Button>
+
+                <Button variant='contained' color='primary'>
+                  <Link
+                    style={{ textDecoration: 'none', color: 'white' }}
+                    to='/login'
+                  >
+                    Login to Your Account
+                  </Link>
+                </Button>
               </div>
             </div>
           </div>
